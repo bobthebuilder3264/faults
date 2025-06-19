5328a521f52c4a66358e322abe3642014337f399
bobthebuilder3264 <viniyanofficial@gmail.com>
2025-06-19 19:42:41 +0800

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
 self.addEventListener('install', e => {
-  const files = ['/', '/index.html', '/manifest.json', '/service-worker.js', '/icon.png'];
+  const files = [
+  '/trial/',
+  '/trial/index.html',
+  '/trial/manifest.json',
+  '/trial/service-worker.js',
+  '/trial/icon.png'
+];
   e.waitUntil(caches.open('fault-cache').then(c => c.addAll(files)));
 });
 self.addEventListener('fetch', e => {
